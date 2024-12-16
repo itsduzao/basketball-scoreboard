0e37bf9b84b5543a942d45efe02580044040fdfc
Eduardo Henrique <itsduzao@outlook.com>
2024-12-16 08:31:38 -0300

    refactor: simplify score button setup by using dedicated setup functions

--- index.js
+++ index.js
@@ -1,39 +1,5 @@
-let homeScoreCounter = 0;
-let guestScoreCounter = 0;
+import { setupHomeScoreButtons } from './setupHomeScoreButtons.js';
+import { setupGuestScoreButtons } from './setupGuestScoreButtons.js';
 
-const homeScoreElement = document.getElementById('home-score');
-const guestScoreElement = document.getElementById('guest-score');
-
-const btnHomeAddOne = document.getElementById('btn-home-add-one');
-const btnHomeAddTwo = document.getElementById('btn-home-add-two');
-const btnHomeAddThree = document.getElementById('btn-home-add-three');
-
-btnHomeAddOne.addEventListener('click', () => {
-  homeScoreCounter += 1;
-  homeScoreElement.textContent = homeScoreCounter;
-});
-btnHomeAddTwo.addEventListener('click', () => {
-  homeScoreCounter += 2;
-  homeScoreElement.textContent = homeScoreCounter;
-});
-btnHomeAddThree.addEventListener('click', () => {
-  homeScoreCounter += 3;
-  homeScoreElement.textContent = homeScoreCounter;
-});
-
-const btnGuestAddOne = document.getElementById('btn-guest-add-one');
-const btnGuestAddTwo = document.getElementById('btn-guest-add-two');
-const btnGuestAddThree = document.getElementById('btn-guest-add-three');
-
-btnGuestAddOne.addEventListener('click', () => {
-  guestScoreCounter += 1;
-  guestScoreElement.textContent = guestScoreCounter
-});
-btnGuestAddTwo.addEventListener('click', () => {
-  guestScoreCounter += 2;
-  guestScoreElement.textContent = guestScoreCounter;
-});
-btnGuestAddThree.addEventListener('click', () => {
-  guestScoreCounter += 3;
-  guestScoreElement.textContent = guestScoreCounter;
-});
+setupHomeScoreButtons()
+setupGuestScoreButtons()
